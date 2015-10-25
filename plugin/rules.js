daaba164544fef1a23fc26ae8cb7694ddb6cea3d
Nicolas Siver <nicolas@siver.im>
2015-10-25 19:04:19 -0400

    memory cache with main parsing function

--- plugin/rules.js
+++ plugin/rules.js
@@ -4,12 +4,36 @@
 (function (Rules) {
     'use strict';
 
+    var database = require('./database');
+
+    var rulesList = [];
+
     Rules.invalidate = function (done) {
-        done();
+        database.getRules(function (error, rules) {
+            if (error) {
+                return done(error);
+            }
+
+            // Re-compile regular expressions
+            rulesList = rules.map(function (rule) {
+                return {
+                    match      : new RegExp(rule.regex, "g"),
+                    replacement: rule.replacement
+                };
+            });
+            done();
+        });
     };
 
     Rules.parse = function (content, done) {
+        var i = 0, len = rulesList.length, rule;
 
+        for (i; i < len; ++i) {
+            rule = rulesList[i];
+            content = content.replace(rule.match, rule.replacement);
+        }
+
+        done(null, content);
     };
 
 })(module.exports);
