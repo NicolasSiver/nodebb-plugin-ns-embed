2031c5d4aaa47c742a99f7144e2431d6558f414e
Nicolas Siver <nicolas@siver.im>
2017-01-11 21:28:40 -0500

    add unit tests for twitch rules

--- test/regexRules.spec.js
+++ test/regexRules.spec.js
@@ -10,8 +10,9 @@ const findRule = (name) => {
 }, repl        = '---';
 
 describe('RegExp Rules', () => {
+
     describe('Youtube', () => {
-        let regex = new RegExp(findRule('youtube').regex, 'g');
+        let regex;
         const regular           = 'https://www.youtube.com/watch?v=Dwrm0X7RL0c',
               regularNoProtocol = 'www.youtube.com/watch?v=Dwrm0X7RL0c',
               regularNoZone     = 'https://youtube.com/watch?v=Dwrm0X7RL0c',
@@ -21,6 +22,10 @@ describe('RegExp Rules', () => {
         const short           = 'https://youtu.be/opgt8ZVqP_g',
               shortNoProtocol = 'youtu.be/opgt8ZVqP_g',
               shortId         = 'opgt8ZVqP_g';
+
+        beforeEach(() => {
+            regex = new RegExp(findRule('youtube').regex, 'g');
+        });
 
         it('parses general urls', () => {
             expect(regular.replace(regex, repl)).to.equal(repl);
@@ -50,4 +55,65 @@ describe('RegExp Rules', () => {
             expect(profileUrl.replace(regex, repl)).to.equal(profileUrl);
         })
     });
+
+    describe('Twitch', () => {
+        let regexLive, regexVod;
+
+        const video          = 'https://www.twitch.tv/chess/v/114095976',
+              channel        = 'https://www.twitch.tv/chess',
+              videoAnchorTag = '<a href="https://www.twitch.tv/astreamer/v/888" target="_blank" rel="nofollow">Some Text</a>',
+              liveAnchorTag  = '<a href="https://www.twitch.tv/astreamer" target="_blank" rel="nofollow">Watch Live</a>',
+              emptyAnchorTag = '<a href="https://www.twitch.tv/astreamer" target="_blank" rel="nofollow"></a>';
+
+        beforeEach(() => {
+            regexLive = new RegExp(findRule('twitch-live').regex, 'g');
+            regexVod = new RegExp(findRule('twitch-vod').regex, 'g');
+        });
+
+        it('parses live url', () => {
+            expect(channel.replace(regexLive, repl)).to.be.equal(repl);
+        });
+
+        it('ignores video url for a live rule', () => {
+            expect(video.replace(regexLive, repl)).to.be.equal(video);
+        });
+
+        it('ignores video url in anchor tag', () => {
+            expect(videoAnchorTag.replace(regexLive, repl)).to.be.equal(videoAnchorTag);
+        });
+
+        it('parses live anchor tag', () => {
+            expect(liveAnchorTag.replace(regexLive, repl)).to.be.equal(repl);
+        });
+
+        it('parses empty live anchor tag', () => {
+            expect(emptyAnchorTag.replace(regexLive, repl)).to.be.equal(repl);
+        });
+
+        it('parses video url', () => {
+            expect(video.replace(regexVod, repl)).to.be.equal(repl);
+        });
+
+        it('ignores live url for a vod rule', () => {
+            expect(channel.replace(regexVod, repl)).to.be.equal(channel);
+        });
+
+        it('ignores live url in anchor tag', () => {
+            expect(liveAnchorTag.replace(regexVod, repl)).to.be.equal(liveAnchorTag);
+        });
+
+        it('parses vod anchor tag', () => {
+            expect(videoAnchorTag.replace(regexVod, repl)).to.be.equal(repl);
+        });
+
+        it('extracts channel name as a first group', () => {
+            expect(regexLive.exec(channel)[1]).to.be.equal('chess');
+        });
+
+        it('extracts video id as a first group', () => {
+            expect(regexVod.exec(video)[1]).to.be.equal('114095976');
+        });
+
+    });
+
 });
