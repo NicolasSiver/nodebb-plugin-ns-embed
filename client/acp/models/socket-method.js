3f7fa3ffecdd73732922fe9a2e1c699cc8eaf314
Nicolas Siver <nicolas@siver.im>
2021-05-31 19:49:10 -0400

    change socket methods to use enum like structure

--- client/acp/models/socket-method.js
+++
@@ -1,7 +0,0 @@
-export default Object.freeze({
-    CREATE_RULE          : 'admin.plugins.ns-embed.ruleCreate',
-    DELETE_RULE          : 'admin.plugins.ns-embed.ruleDelete',
-    GET_ALL_RULES        : 'admin.plugins.ns-embed.embedRulesGet',
-    INSTALL_DEFAULT_RULES: 'admin.plugins.ns-embed.defaultRulesInstall',
-    SAVE_RULE            : 'admin.plugins.ns-embed.ruleSave'
-});
