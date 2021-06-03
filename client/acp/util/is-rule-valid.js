48a65354be2d546a50e145f2e61400e2c5a1ff33
Nicolas Siver <nicolas@siver.im>
2021-06-02 22:59:02 -0400

    change to handle undefined value

--- client/acp/util/is-rule-valid.js
+++ client/acp/util/is-rule-valid.js
@@ -1,6 +1,6 @@
 export function isRuleValid(...fields) {
     for (let field of fields) {
-        if (field === null || typeof field === 'string' && field.length === 0) {
+        if (field === null || field === undefined || typeof field === 'string' && field.length === 0) {
             return false;
         }
     }
